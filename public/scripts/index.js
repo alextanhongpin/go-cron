6840fb3861f55ed782b2b6e5f6baccd20d78def4
Hong Pin Alex Tan <alextan@administrators-MacBook-Pro.local>
2017-07-05 13:55:21 +0800

    Remove unused comments

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -63,62 +63,13 @@
         }
       }
     },
-    // ids: ['prev', 'next', 'start', 'current', 'executing', 'running', 'version', 'counter', 'name', 'spec', 'progress', 'status', 'start-exec', 'end-exec', 'info-exec'],
-    // isClosed: false, // Bool to check if the socket is closed
-    // counter: 0, // Int that records the time
-    // throttle: 500, // Int in milliseconds the time to debounce the request animation frame
-    // isThrottled () {
-    //   return this.counter < this.throttle
-    // },
-    // increment (value) {
-    //   this.counter += value
-    // },
     reset () {
-      // this.counter = 0
-      // this.data.job_start_time = null
-      // this.data.job_end_time = null
-      // this.data.prev_execution = null
-      // this.data.next_execution = null
       this.set('counter', 0)
       this.set('job_start_time', null)
       this.set('job_end_time', null)
       this.set('prev_execution', null)
       this.set('next_execution', null)
     },
-    // data: {},
-    // getProgressPercentage () {
-    //   if (!this.data.prev_execution || !this.data.prev_execution) {
-    //     return '0%'
-    //   }
-    //   const prevExecutionTime = this.formatTime(this.data.prev_execution)
-
-    //   if (prevExecutionTime.trim() === '08:00:00 am') {
-    //     return '0%'
-    //   }
-    //   const a = new Date(this.data.prev_execution).getTime()
-    //   const b = new Date(this.data.next_execution).getTime()
-    //   const c = Date.now()
-    //   return this.toPercentage(Math.ceil((c - a) / (b - a) * 100))
-    // },
-    // getJobDuration () {
-    //   const start = this.data.job_start_time
-    //   const end = this.data.job_end_time
-
-    //   const seconds = (new Date(end).getTime() - new Date(start).getTime()) / 1000
-    //   if (!seconds) {
-    //     return 'N/A'
-    //   }
-    //   // Can I convert it to minutes?
-    //   const minutes = Math.floor(seconds / 60)
-    //   if (minutes > 1) {
-    //     return `${minutes}min ${seconds}s`
-    //   }
-    //   const hours = Math.floor(seconds / (60 * 60))
-    //   if (hours > 1) {
-    //     return `${hours}hr ${minutes}min ${seconds}s`
-    //   }
-    //   return `${seconds}s`
-    // },
     formatTime (time) {
       if (!time) {
         return '-'
